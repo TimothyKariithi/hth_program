c67016fbb304a6c6b57940635c8d179cb3948dbb
TimothyKariithi <timothy.kariithi@hgs.hiddengeniusproject.org>
2022-10-27 17:30:35 -0700

    updating first assignment

---
+++ Getting-Started-with-Python-TimothyKariith2-2/About_Me.py
@@ -0,0 +1,15 @@
+
+full_name = "Timothy Kariiti"
+nickname = "none"
+age = 15
+has_used_python = True
+
+hobbies = ["playing music", "riding my bike", "cooking/baking", "watching shows", "playing car games"]
+
+fav_things = {
+  "food": "Pasta",
+  "hobby": hobbies[0],
+  "car" : ["Bugatti Chiron", "GTR", "Porshe 911"],
+}
+
+all_vars = dict(vars())
